62b4a6515ac1f54487e9892ed99718848ff08311
Maria <mjgarza07@gmail.com>
2020-01-13 11:21:38 -0700

    HelloWorld

---
+++ Project real 1/Assets/Scripts/NewClass.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewClass : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+        print(message: "Hello World");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print(message: "Hello World");
+    }
+    
+}
