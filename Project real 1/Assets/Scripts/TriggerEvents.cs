1395dac28d67df71f8f6fe5f44d503d9df69a47b
Maria <mjgarza07@gmail.com>
2020-01-27 11:18:46 -0700

    trigger

---
+++ Project real 1/Assets/Scripts/TriggerEvents.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+public class TriggerEvents : MonoBehaviour
+{
+    public UnityEvent triggerEnterEvent;
+    private void OnTriggerEnter(Collider other)
+    {
+        triggerEnterEvent.Invoke();
+    }
+}
