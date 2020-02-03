2b24479cc076cc30dab689f8e206a6af60d3fcd5
Maria <mjgarza07@gmail.com>
2020-02-03 11:23:40 -0700

    game player movement

---
+++ Project real 1/Assets/Scripts/CharacterMovemnt.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovemnt : MonoBehaviour
+{
+    public CharacterController controller;
+    private Vector3 positionDirection;
+    public float speed = 5f;
+    public float gravity = -3f;
+    public float jumpForce = 10f;
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        positionDirection.x = Input.GetAxis("Horizontal")*speed;
+        if (Input.GetButtonDown("Jump") && controller.isGrounded)
+        {
+            positionDirection.y = jumpForce;
+        }
+        positionDirection.y += gravity;
+        controller.Move(positionDirection*Time.deltaTime);
+    }
+}
