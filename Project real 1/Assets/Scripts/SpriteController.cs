f852b5222036a9774a1bfc03eed320e900797970
Maria <mjgarza07@gmail.com>
2020-02-12 11:19:51 -0700

    controlling components

---
+++ Project real 1/Assets/Scripts/SpriteController.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+using UnityEngine;
+[RequireComponent(typeof(SpriteRenderer))]
+public class SpriteController : MonoBehaviour
+{
+    public Color spriteColor = Color.red;
+    public Color offColor = Color.red;
+    private SpriteRenderer spriteObj;
+    
+    void Start()
+    {
+        spriteObj = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnEnable()
+    {
+        spriteObj.color = spriteColor;
+    }
+
+    private void OnDisable()
+    {
+        spriteObj.color = offColor;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
