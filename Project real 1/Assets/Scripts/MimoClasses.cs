b3bea11a80a52dc6bfe02b7e37f618f97ab80da2
Maria <mjgarza07@gmail.com>
2020-02-18 19:54:01 -0700

    MIMO classes

---
+++ Project real 1/Assets/Scripts/MimoClasses.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+
+public class MimoClasses : MonoBehaviour
+public class Count;
+
+{
+// Start is called before the first frame update
+    void Start()
+    List<int> list = new List<int>();
+    Console.Writeline(List.Count);
+
+    List<int> list = new List<int>();
+    List.Add (42);
+    List.Insert (9000, 1);
+    List.RemoveAt (1);
+    Console.Writeline (List.Count);
+}
+
+public class Car
+{
+    string color;
+    void StartEngine();
+    {
+        Console.WriteLine("Vroom!");
+    }
+}
+
+Car myCar = new Car();
+myCar.StartEngine ();
+myCar.color = "red";
+
+{
+internal string colour;
+
+Car(string color)
+{
+    this.color = Color;
+    StartEngine();
+}
+
+void StartEngine();
+{
+    Console.WriteLine("Vroom!");
+}
+
+Car mycar = new Car("red");
+
+{
+    string GetDetails()
+    {
+        return "This car is " + color;
+    }
+}
+
+internal Car myCar = new Car("red");
+myCar.GetDetails();
+
+private string color;
+
+public string Color
+{
+    get { return color; }
+    set { color = value; }
+}
+Car vehicle = new Car();
+vehicle.Color = "Blue";
+Console.Writeline;
+
+public class Vehicle
+{
+    public int wheels = 4;
+    public string brand = "Ford";
+}
+
+public class Car : Vehicle
+{
+    public string GetDetails()
+    {
+        return "This is a " + brand + "with" + wheels "wheels";
+    }
+}
+Car myCar = new Car();
+Console.WriteLine (myCar.GetDetails());
+
+    
+
+// Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
