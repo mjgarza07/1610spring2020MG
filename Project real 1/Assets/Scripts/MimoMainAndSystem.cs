c0835e66b213e88f00e4f92c7573b76d8a95d43b
Maria <mjgarza07@gmail.com>
2020-02-19 11:42:57 -0700

    MIMO main and systems

---
+++ Project real 1/Assets/Scripts/MimoMainAndSystem.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections.Generic;
+public class MimoMainAndSystem : MonoBehaviour;
+
+namespace App
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            SayHi();
+        }
+
+        static void SayHi()
+        {
+            Console.WriteLine("Hi!");
+        }
+    }
+
+    {
+        foreach (String arg in args)
+        {
+            Console.WriteLine(arg);
+        }
+    internal var c = new Classes.car("red");
+    }
+    {
+    internal var n = new;
+    System.Collections.Generic.List<int>();
+    internal var m = new List<>();
+        n.Add(8);
+        m.Add(1);
+        m.Add(n[0]);
+    internal string s = String.Join(",", m);
+    Console.Write(s);
+    }
+}
+
+
