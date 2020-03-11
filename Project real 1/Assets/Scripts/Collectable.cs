52d893979011aa5d2aecdcfee1f2cebe6dc025f6
Maria <mjgarza07@gmail.com>
2020-03-11 11:13:32 -0600

    idk bro i did something

---
+++ Project real 1/Assets/Scripts/Collectable.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class Collectable : ScriptableObject
+{
+    public int powerLevel = 10;
+    public Color collectableColor = Color.red;
+    public Sprite collectableSprite;
+}
---
+++ Project real 1/Assets/Scripts/ListsAndLoops.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+[CreateAssetMenu]
+public class ListsAndLoops : ScriptableObject
+{
+    
+    
+    public List<string> stringList;
+    public string singleString;
+    
+    
+
+
+    public void AddToList(string stringObj)
+    {
+        stringList.Add(stringObj);
+    }
+
+    public void RemoveFromList(string stringObj)
+    {
+        for (int i = 0; i < stringList.Count; i++)
+        {
+            if (stringList[i] == stringObj)
+            {
+                stringList.Remove(stringObj);
+            }
+        }
+    }
+
+    public void SortList()
+    {
+        stringList.Sort();
+    }
+    
+    
+    public void CheckLst()
+    {
+        foreach (var obj in stringList)
+        {
+            if (obj == singleString);
+            {
+                Debug.Log(obj);
+            }
+        }
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
